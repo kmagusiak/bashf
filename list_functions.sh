6d9e55b232d4958ed18d239043b1229c7f317f59
Krzysztof Magusiak <chrmag@poczta.onet.pl>
2018-01-12 22:01:48 +0100

    List functions example script.

---
+++ list_functions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function usage() {
+	cat <<EOF
+Usage: $0 file
+List functions defined in a bash file.
+EOF
+}
+source ./bashf.sh || exit 1
+
+log_start "$@"
+
+function show_file_functions() {
+	local file="$1"
+	log_info "Listing functions of $file"
+	(grep -n '^\(function \)\?[a-z]\w*()' "$file" || true) |
+		sed 's/^\([0-9]\+\):\(function\s*\)\?\([a-z]\w*\)().*$/\3:\1/' |
+		sort | indent
+}
+
+[ $# -eq 0 ] && die_usage "No parameters provided"
+while [ $# -gt 0 ]
+do
+	case "$1" in
+	-h|--help)
+		usage
+		exit;;
+	-*)
+		die "Invalid option [$1]"
+		;;
+	*)
+		show_file_functions "$1"
+		shift
+		;;
+	esac
+done
+log_debug "Finished."
