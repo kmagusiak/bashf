926827672bbf6dd1d20aba7ef7d8a384225c502b
Krzysztof Magusiak <chrmag@poczta.onet.pl>
2019-01-27 16:58:37 +0100

    Read functions: arguments added

--- list_functions.sh
+++ list_functions.sh
@@ -5,16 +5,47 @@
 # try sourcing locally first
 source ./bashf.sh || source bashf.sh || exit 1
 
+SHOW_DETAILS=N
+FILTER=
+
+function show_file_function() {
+	# $1: file
+	# $2: function name
+	local func=${2%:*}
+	local line=${2#*:}
+	printf "%s ${COLOR_DIM}(%d)${COLOR_RESET}\n" "$func" "$line"
+	if ! has_flag SHOW_DETAILS
+	then
+		echo "$2"
+		return
+	fi
+	(
+		sed -n "/func.*$func\(\)/q"
+		# TODO read comments
+	) | indent < "$1"
+}
 function show_file_functions() {
 	local file="$1"
 	[ -r "$file" ] || die "Cannot read file [$file]"
 	log_info "Listing functions of $file"
-	(grep -n '^\(function \)\?[a-z]\w*()' "$file" || true) |
+	local f functions=($(
+		(grep -n '^\(function \)\?[a-z]\w*()' "$file" || true) |
 		sed 's/^\([0-9]\+\):\(function\s*\)\?\([a-z]\w*\)().*$/\3:\1/' |
-		sort | indent
+		sort
+		))
+	for f in "${functions[@]}"
+	do
+		[ -z "$FILTER" ] || [[ "$f" == $FILTER ]] || continue
+		show_file_function "$file" "$f"
+	done
+	return 0
 }
 
 log_start "$@"
+arg_parse_opt details 'Show documentation' \
+	-v SHOW_DETAILS -f
+arg_parse_opt filter 'Filter functions' \
+	-v FILTER -r -s f
 arg_parse_rest files
 arg_parse_require 1
 arg_parse "$@"
